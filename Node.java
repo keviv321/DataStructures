e51b2758807654ed8a74f5f98a07e350d42a86c4
VIVEK KUMAR <32438255+vivekvvk143@users.noreply.github.com>
2018-09-04 23:36:07 +0530

    Add files via upload

---
+++ Node.java
@@ -0,0 +1,14 @@
+package HackerRank;
+
+public class Node {
+	Integer data,height;
+	public Node left;
+	Node right;
+	public Node(Integer item) {
+		//super();
+		data = item;
+		left=right=null;
+		height=0;
+	}
+
+}
