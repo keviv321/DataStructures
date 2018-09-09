1d364b11ed163767ff9ebb658884b2ae9993f2af
VIVEK KUMAR <32438255+vivekvvk143@users.noreply.github.com>
2018-09-09 16:23:08 +0530

    Update QueueUsingTwoStacks.java

--- QueueUsingTwoStacks.java
+++ QueueUsingTwoStacks.java
@@ -1,82 +1,43 @@
-package HackerRank;
-
-//import java.util.LinkedList;
-//import java.util.Queue;
+package DS;
 import java.util.Scanner;
 import java.util.Stack;
 
-@SuppressWarnings("serial")
-public class QueueUsingTwoStacks {
-	//Stack <Integer> Q;
+class Queue
+{
+
+	private Stack<Integer> q = new Stack<Integer>();
+	private Stack<Integer> temp = new Stack<Integer>();
 
 
-
-	public static void main(String args[])
+	public void enqueue(int val)
 	{
-		Queue enqueue = new Queue();
-		Scanner in=new Scanner(System.in);
-		int queries=in.nextInt();
-		int choice;Integer value;
-		while(queries-->0)
-		{
-			//ob=new QueueUsingTwoStacks();
-			//ob.Q=new Stack<Integer>();
-			choice=in.nextInt();
-			if (choice==1)
-			{
-				value=in.nextInt();
-				enqueue.push(value);
-			}
-			else if(choice==2)
-			{
-				enqueue.pop();
-			}
-			else if(choice==3)
-				System.out.println(enqueue.peek());//ob.Q.peek();
-		}
-
-		in.close();
-	}
-}
-
-class Queue extends Stack<Integer>{
-
-	@Override
-	public  Integer peek() {
-		// TODO Auto-generated method stub
-		Stack <Integer> dequeue=new Stack <Integer>();
-
-		while(!super.isEmpty())
-		{
-			dequeue.push(super.pop());
-		}
-		Integer ob = dequeue.peek();
-		while(!dequeue.isEmpty())
-		{
-			this.push(dequeue.pop());
-		}
-		return ob;
+		q.push(val);
 	}
 
+	public void dequeue()
+	{
 
+		if(temp.isEmpty()){
+			while (!q.isEmpty())
+			{
+				temp.push(q.pop());
+			}
+		}
+		temp.pop();
 
-	@Override
-	public  Integer pop() {
-		// TODO Auto-generated method stub
-		Stack <Integer> obj=new Stack <Integer>();
-		Integer i;
-		while(!this.isEmpty())
+	}
+
+	public Integer printHead()
+	{
+
+		if(temp.isEmpty())
 		{
-			i=super.pop();
-			obj.push(i);
+			while (!q.isEmpty())
+			{
+				temp.push(q.pop());
+			}
 		}
-		Integer o=obj.pop();
-		while(!obj.isEmpty())
-		{
-			i=obj.pop();
-			super.push(i);
-		}
-		return o;
+		return temp.peek();
 	}
 
 }
